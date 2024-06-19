53d14a1d89599b43e57c2c7bab5a330b5c1d8bd0
Rosemaryflavor <weesy44@naver.com>
2024-06-19 09:29:28 +0900

    2024.06.19 이클립스 최초 커밋

---
+++ java_00/src/java_00/HelloJava.java
@@ -0,0 +1,14 @@
+package java_00;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		System.out.print("Hello 자바링 !!!");
+        	
+	}
+
+}
