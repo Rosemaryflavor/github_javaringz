a04e30b7f4004f1f1ca6f023e6c93ad31fa49dfb
Rosemaryflavor <weesy44@naver.com>
2024-07-08 12:49:16 +0900

    0708 비 내리는 아침~ 한 주의 시작~

---
+++ java_00/src/java_11/phoneBook/dao/PhoneBookDAO.java
@@ -0,0 +1,30 @@
+package java_11.phoneBook.dao;
+
+import java.util.HashMap;
+
+import java_11.phoneBook.model.Phone;
+
+public class PhoneBookDAO {
+
+	public boolean dupCheck(String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public HashMap<String, Phone> searchAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Phone search(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void update(String name, String phoneNum, String addr) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
---
+++ java_00/src/java_11/phoneBook/model/Phone.java
@@ -0,0 +1,36 @@
+package java_11.phoneBook.model;
+
+public class Phone {
+
+	public Phone(String name, String tel, String addr) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setTel(String tel) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setAddr(String addr) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getTel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getAddr() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
